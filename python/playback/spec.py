7c6fc7ce1ee484e018218d0cb8e2ee40fca8cf2a
NeverNotJohn <johntheherrera@gmail.com>
2023-02-18 22:27:51 -0800

    added general time series plot to spec.py

--- python/playback/spec.py
+++ python/playback/spec.py
@@ -18,8 +18,20 @@ print(ch_2)
 
 
 """   Create Spectogram   """
-plt.specgram(ch_2.values, NFFT=256, Fs=200, cmap="rainbow")
+
+plt.specgram(ch_2.values, NFFT=512, Fs=200, cmap="rainbow")
 plt.colorbar()
 plt.ylabel("Freq (hz)")
 plt.xlabel("Time (s)")
+plt.show()
+
+"""   Create Time Series Plot   """
+
+dt = (df.index.values.tolist())         # dt = change in time... x-axis of plot
+for i in dt:                            # dt[index] = index / frequency of data collection
+    dt[i] = i / 200                     # ganglion records 200 data points a second (200 hz)
+
+plt.plot(dt, ch_2)                
+plt.ylabel("Freq (hz)")
+plt.xlabel("Time (s)")              
 plt.show()
