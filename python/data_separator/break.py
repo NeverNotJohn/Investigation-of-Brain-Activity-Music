bee699945e78b405dd3dc0a67499cacdfbb161d6
NeverNotJohn <johntheherrera@gmail.com>
2023-03-14 19:23:25 -0700

    added .csv data breaking yay pog

--- python/data_separator/break.py
+++ python/data_separator/break.py
@@ -1,27 +1,37 @@
 import pandas as pd
 
-"""   Read File and separate to different channels   """
+"""
 
-file_name = "OpenBCI-RAW-2023-03-07_18-30-02-DS-Alpha-T4.csv"                    # change file name here
-path = f"raw_data\{file_name}"
+    Time Sections:
 
+    sleep: 0 - 1 min
+    music: 1 - 5 min
+    sleep: ...
+
+    Notes:
+        Skip first 5 secs
+"""
+
+"""   Read File   """
+
+file_name = "OpenBCI-RAW-2023-03-07_18-30-02-DS-Alpha-T4"                        # change file name here
+path = f"raw_data\{file_name}.csv"
 
 df = pd.read_csv(path, sep=",", skiprows=4)
-
-print(df)
-
 sample_rate = 200
 
-lower_bounds = (int(input("Input lower bounds in seconds: "))) * sample_rate     # seconds input * sample_rate hz sample rate
-upper_bounds = (int(input("Input upper bounds: "))) * sample_rate  
+""" Time sections """
+
+lower_bounds = 5 * sample_rate                                                   # seconds input * sample_rate hz sample rate
+upper_bounds = 60 * sample_rate     
+
+sleep_one = df.iloc[lower_bounds:upper_bounds, [2,4]]                            # separate into single dataframe
+
+sleep_one.to_csv(f"sleep_one\{file_name}_sleep_one.csv")                         # write file
 
 
-ch_2 = df.loc[lower_bounds:upper_bounds, " EXG Channel 1"]
-ch_4 = df.loc[lower_bounds:upper_bounds, " EXG Channel 3"]
+""" WRITE CSV FILES """
 
 
-ch_2 = ch_2.to_frame()
 
-print(ch_2)
 
-ch_2.to_csv('sleep/out.csv')
