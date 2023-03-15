e1d3b91082e10a723a6a54a4cd3d673a97915a0b
NeverNotJohn <johntheherrera@gmail.com>
2023-03-14 19:31:02 -0700

    added 4 time sections

--- python/data_separator/break.py
+++ python/data_separator/break.py
@@ -10,6 +10,7 @@ import pandas as pd
 
     Notes:
         Skip first 5 secs
+        1 second buffer
 """
 
 """   Read File   """
@@ -20,18 +21,39 @@ path = f"raw_data\{file_name}.csv"
 df = pd.read_csv(path, sep=",", skiprows=4)
 sample_rate = 200
 
-""" Time sections """
+""" Time section 1 - Sleep """
 
 lower_bounds = 5 * sample_rate                                                   # seconds input * sample_rate hz sample rate
 upper_bounds = 60 * sample_rate     
 
 sleep_one = df.iloc[lower_bounds:upper_bounds, [2,4]]                            # separate into single dataframe
 
-sleep_one.to_csv(f"sleep_one\{file_name}_sleep_one.csv")                         # write file
+sleep_one.to_csv(f"sep_data\sleep_one\{file_name}_sleep_one.csv")                         # write file
 
 
-""" WRITE CSV FILES """
+""" Time Section 2 - Music """
 
+lower_bounds = 61 * sample_rate                                                   # seconds input * sample_rate hz sample rate
+upper_bounds = 300 * sample_rate     
 
+sleep_one = df.iloc[lower_bounds:upper_bounds, [2,4]]                            # separate into single dataframe
 
+sleep_one.to_csv(f"sep_data\music_one\{file_name}_music_one.csv")                         # write file
 
+""" Time Section 3 - Sleep """
+
+lower_bounds = 301 * sample_rate                                                   # seconds input * sample_rate hz sample rate
+upper_bounds = 360 * sample_rate     
+
+sleep_one = df.iloc[lower_bounds:upper_bounds, [2,4]]                            # separate into single dataframe
+
+sleep_one.to_csv(f"sep_data\sleep_two\{file_name}_sleep_two.csv")                         # write file
+
+""" Time section 4 - Music """
+
+lower_bounds = 361 * sample_rate                                                   # seconds input * sample_rate hz sample rate
+upper_bounds = 660 * sample_rate     
+
+sleep_one = df.iloc[lower_bounds:upper_bounds, [2,4]]                            # separate into single dataframe
+
+sleep_one.to_csv(f"sep_data\music_two\{file_name}_music_two.csv")                         # write file
